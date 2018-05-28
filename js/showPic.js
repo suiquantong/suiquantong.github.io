e136e28dd324aac2f6b37b5d54a78b13caf3da21
suiquantong <37520861+suiquantong@users.noreply.github.com>
2018-05-28 17:59:46 +0800

    Add files via upload

---
+++ js/showPic.js
@@ -0,0 +1,27 @@
+function showPic(whichpic) {
+	var source = whichpic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src", source);
+	var text = whichpic.getAttribute("title");
+	var description = document.getElementById("description");
+	description.firstChild.nodeValue = text;
+}
+function prepareGallery() {
+	var links = document.getElementsByTagName("a");
+	for ( var i = 0; i < links.length; i++) {
+		links[i].onclick = function() {
+			showPic(this);
+			return false;
+		}
+	}
+}
+function addLoadEvent() {
+	var oldonload = window.onload;
+	if (typeof oldonload == 'function') {
+		oldonload();
+		prepareGallery();
+	} else {
+		window.onload = prepareGallery;
+	}
+}
+addLoadEvent();
